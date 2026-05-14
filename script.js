7218bac7a2254842065cd4bd34066f9dd3c4027f
anishapriya <anisharoyanjlina@gmail.com>
2026-05-14 09:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ console.log(byteSize('hello world')); // 11
 console.log(byteSize('안녕하세요'));   // 15
 console.log(byteSize(''));            // 0
 	
-	function byteSize(str) {
-    return new TextEncoder().encode(str).length;
-}
+
 
 // Examples
 console.log(byteSize('hello world')); // 11
