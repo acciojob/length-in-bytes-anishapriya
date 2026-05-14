0c38f5d60817355c18bb8edbc7de61d2eff6fecc
anishapriya <anisharoyanjlina@gmail.com>
2026-05-14 08:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const byteSize = (str) => {
   // write your code here
+	const byteSize = (str) => {
+    return new TextEncoder().encode(str).length;
+}
+// Examples
+console.log(byteSize('hello world')); // 11
+console.log(byteSize('안녕하세요'));   // 15
+console.log(byteSize(''));            // 0
+	
+	function byteSize(str) {
+    return new TextEncoder().encode(str).length;
+}
+
+// Examples
+console.log(byteSize('hello world')); // 11
+console.log(byteSize('안녕하세요'));   // 15
+console.log(byteSize(''));            // 0
+	
 };
 
 // Do not change the code below
